9afdb3b5849a74fd2c3d2b2f751851eec2695fbf
revuer <jazz.homi@gmail.com>
2025-06-13 09:21:48 +0530

    done

--- app/our-businesses/gemstones/page.js
+++ app/our-businesses/gemstones/page.js
@@ -1,13 +1,12 @@
 
-import Breadcrumb from '@/components/Breadcrumb';
-import Precious from '@/components/gemstones/Precious';
-import SemiPrecious from '@/components/gemstones/SemiPrecious';
-import SourcingWorld from '@/components/gemstones/SourcingWorld';
-import GemstoneManufacturing from '@/components/gemstones/GemstoneManufacturing';
-import DistributionMap from '@/components/gemstones/DistributionMap';
-import Contact from '@/components/gemstones/Contact';
+import Breadcrumb from '@/components/Breadcrumb/index.jsx';
+import Precious from '@/components/gemstones/Precious/index.jsx';
+import SemiPrecious from '@/components/gemstones/SemiPrecious/index.jsx';
+import SourcingWorld from '@/components/gemstones/SourcingWorld/index.jsx';
+import GemstoneManufacturing from '@/components/gemstones/GemstoneManufacturing/index.jsx';
+import DistributionMap from '@/components/gemstones/DistributionMap/index.jsx';
+import Contact from '@/components/gemstones/Contact/index.jsx';
 
-import VerticalStepNav from '@/components/gemstones/VerticalStepNav';
 
 import bgImage from '@/assets/images/banners/gemstones-banner-1.jpg';
 
